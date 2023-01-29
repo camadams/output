3c2be3fe95a696b2efc3bcca2b6597ede6118159
cameron.adams <camgadams@gmail.com>
2023-01-29 11:35:20 +0200

    importing projects

---
+++ projects/pathfinding/Node.js
@@ -0,0 +1,49 @@
+class Node {
+  static CLOSED = 0;
+  constructor(x, y, r, c) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.c = c;
+    this.neighbours = [];
+    this.cutOff = [];
+    this.state = 'none';
+    this.prev = null;
+    this.minCostFromStart = Number.MAX_VALUE;
+  }
+
+  show() {
+    if (this.state == 'start') {
+      fill(0, 255, 0);
+    } else if (this.state == 'rearrived') {
+      fill(255, 140, 0);
+    } else if (this.state == 'end') {
+      fill(255, 0, 0);
+    } else if (this.state == 'closed') {
+      if (debug) {
+        line(this.x - 10, this.y - 10, this.x + 10, this.y + 10);
+        line(this.x - 10, this.y + 10, this.x + 10, this.y - 10);
+      }
+      fill(0, 0, 255);
+    } else if (this.state == 'open') {
+      noFill();
+      ellipse(this.x, this.y, 20, 20);
+      fill(128);
+    } else {
+      fill(255);
+    }
+    stroke(40);
+    //strokeWeight(0.9);
+    ellipse(this.x, this.y, 7);
+
+    if (debug) {
+      fill(0);
+      textSize(10);
+      text('g:' + this.g + ' h:' + int(this.h) + ' f:' + int(this.f), this.x + 10, this.y);
+    }
+  }
+
+  toString() {
+    return 'x:' + this.x + ' y:' + this.y + ' this.f:' + this.f;
+  }
+}
---
+++ projects/waves/Wave.js
@@ -0,0 +1,29 @@
+class Wave {
+  constructor(x, y, amp, period) {
+    this.x = x;
+    this.y = y;
+    this.amp = amp;
+    this.points = [];
+    this.period = period;
+    this.generatePoints();
+  }
+
+  generatePoints() {
+    var angle = 0;
+    this.points = [];
+    for (let i = 0; i < width; i++) {
+      const f = -1 * sin(angle) * this.amp;
+      this.points.push(f);
+      angle += (this.period / width) * 2 * Math.PI;
+    }
+  }
+
+  show() {
+    noFill();
+    beginShape();
+    for (let i = 0; i < this.points.length; i++) {
+      vertex(i + this.x, this.points[i] + this.y);
+    }
+    endShape();
+  }
+}
