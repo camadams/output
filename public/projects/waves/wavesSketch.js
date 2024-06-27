11ff4b333fc8c20dccade13ea59b8fd2ed57c8de
cameron.adams <camgadams@gmail.com>
2024-06-27 23:26:33 +0200

    changes

--- public/projects/waves/wavesSketch.js
+++ public/projects/waves/wavesSketch.js
@@ -1,21 +1,26 @@
 var parameterDot;
-const SELECTED_COLOR = 'bg-sky-200';
+const SELECTED_COLOR = "bg-sky-200";
 
 function setup() {
   ////////////////// Working with the DOM (not used for this OpenProcessing project) ///////////////////
   var canvas = createCanvas(WEBGL);
-  canvas.parent('project-container');
-  resizeCanvas(document.getElementById('project-container').clientWidth, document.getElementById('project-container').clientWidth);
+  canvas.parent("project-container");
+  resizeCanvas(
+    document.getElementById("project-container").clientWidth,
+    document.getElementById("project-container").clientWidth
+  );
   parameterDot = new ParameterDot(round(width / 2), round(height / 2));
   initProgressBar();
 }
 function draw() {
-  frameRateDiv.innerHTML = 'Frame Rate: ' + Math.round(frameRate());
+  frameRateDiv.innerHTML = "Frame Rate: " + Math.round(frameRate());
   background(230);
   var ySpacing = height / (parameterDot.numWaves + 1);
   var ampIncrement = parameterDot.maxAmp / (parameterDot.numWaves - 1);
   for (let i = 0; i < parameterDot.numWaves; i++) {
-    new Wave(0, ySpacing * (i + 1), 5 + i * ampIncrement, 5 + i * parameterDot.periodIncrement).show();
+    // var wave = new Wave(0, ySpacing * (i + 1), 5 + i * ampIncrement, 5 + i * parameterDot.periodIncrement);
+    var wave = new Wave(parameterDot, i);
+    wave.show();
   }
   parameterDot.update();
   parameterDot.show();
@@ -91,59 +96,119 @@ function updateProgressBar() {
 }
 
 function clickedOnParameter(id) {
-  var oldParamId = '';
+  var oldParamId = "";
   var selectedParameter = id.slice(0, -1);
-  var isXParam = id.slice(-1) === 'X';
+  var isXParam = id.slice(-1) === "X";
 
   if (isXParam) {
     if (selectedParameter === parameterDot.paramY) return;
-    oldParamId = parameterDot.paramX + 'X';
+    oldParamId = parameterDot.paramX + "X";
     parameterDot.paramX = selectedParameter;
     switch (selectedParameter) {
       case ParameterDot.NUM_WAVES:
-        parameterDot.x = map(parameterDot.numWaves, ParameterDot.NUM_WAVES_MIN, ParameterDot.NUM_WAVES_MAX, 0, width);
+        parameterDot.x = map(
+          parameterDot.numWaves,
+          ParameterDot.NUM_WAVES_MIN,
+          ParameterDot.NUM_WAVES_MAX,
+          0,
+          width
+        );
         break;
       case ParameterDot.PERIOD_INCREMENT:
-        parameterDot.x = map(parameterDot.periodIncrement, ParameterDot.PERIOD_INCREMENT_MIN, ParameterDot.PERIOD_INCREMENT_MAX, 0, width);
+        parameterDot.x = map(
+          parameterDot.periodIncrement,
+          ParameterDot.PERIOD_INCREMENT_MIN,
+          ParameterDot.PERIOD_INCREMENT_MAX,
+          0,
+          width
+        );
         break;
       case ParameterDot.MAX_AMP:
-        parameterDot.x = map(parameterDot.maxAmp, ParameterDot.MAX_AMP_MIN, ParameterDot.MAX_AMP_MAX, 0, width);
+        parameterDot.x = map(
+          parameterDot.maxAmp,
+          ParameterDot.MAX_AMP_MIN,
+          ParameterDot.MAX_AMP_MAX,
+          0,
+          width
+        );
         break;
     }
   } else {
     if (selectedParameter === parameterDot.paramX) return;
-    oldParamId = parameterDot.paramY + 'Y';
+    oldParamId = parameterDot.paramY + "Y";
     parameterDot.paramY = selectedParameter;
     switch (selectedParameter) {
       case ParameterDot.NUM_WAVES:
-        parameterDot.y = map(parameterDot.numWaves, ParameterDot.NUM_WAVES_MIN, ParameterDot.NUM_WAVES_MAX, 0, height);
+        parameterDot.y = map(
+          parameterDot.numWaves,
+          ParameterDot.NUM_WAVES_MIN,
+          ParameterDot.NUM_WAVES_MAX,
+          0,
+          height
+        );
         break;
       case ParameterDot.PERIOD_INCREMENT:
-        parameterDot.y = map(parameterDot.periodIncrement, ParameterDot.PERIOD_INCREMENT_MIN, ParameterDot.PERIOD_INCREMENT_MAX, 0, height);
+        parameterDot.y = map(
+          parameterDot.periodIncrement,
+          ParameterDot.PERIOD_INCREMENT_MIN,
+          ParameterDot.PERIOD_INCREMENT_MAX,
+          0,
+          height
+        );
         break;
       case ParameterDot.MAX_AMP:
-        parameterDot.y = map(parameterDot.maxAmp, ParameterDot.MAX_AMP_MIN, ParameterDot.MAX_AMP_MAX, 0, height);
+        parameterDot.y = map(
+          parameterDot.maxAmp,
+          ParameterDot.MAX_AMP_MIN,
+          ParameterDot.MAX_AMP_MAX,
+          0,
+          height
+        );
         break;
     }
   }
 
   var oldParam = document.getElementById(oldParamId);
-  oldParam.className = oldParam.className.replace(SELECTED_COLOR, '');
+  oldParam.className = oldParam.className.replace(SELECTED_COLOR, "");
 
   var newParam = document.getElementById(id);
-  newParam.className += ' ' + SELECTED_COLOR;
+  newParam.className += " " + SELECTED_COLOR;
 }
 
 function initProgressBar() {
-  var numWavesPercentage = round(map(parameterDot.numWaves, ParameterDot.NUM_WAVES_MIN, ParameterDot.NUM_WAVES_MAX, 0, 100));
+  var numWavesPercentage = round(
+    map(
+      parameterDot.numWaves,
+      ParameterDot.NUM_WAVES_MIN,
+      ParameterDot.NUM_WAVES_MAX,
+      0,
+      100
+    )
+  );
   numWavesFill.style.width = `${numWavesPercentage}%`;
   numWavesText.textContent = `${numWavesPercentage}%`;
 
-  var periodPercentage = round(map(parameterDot.periodIncrement, ParameterDot.PERIOD_INCREMENT_MIN, ParameterDot.PERIOD_INCREMENT_MAX, 0, 100));
+  var periodPercentage = round(
+    map(
+      parameterDot.periodIncrement,
+      ParameterDot.PERIOD_INCREMENT_MIN,
+      ParameterDot.PERIOD_INCREMENT_MAX,
+      0,
+      100
+    )
+  );
   periodFill.style.width = `${periodPercentage}%`;
   periodText.textContent = `${periodPercentage}%`;
 
-  var maxAmpPercentage = round(map(parameterDot.maxAmp, ParameterDot.MAX_AMP_MIN, ParameterDot.MAX_AMP_MAX, 0, 100));
+  var maxAmpPercentage = round(
+    map(
+      parameterDot.maxAmp,
+      ParameterDot.MAX_AMP_MIN,
+      ParameterDot.MAX_AMP_MAX,
+      0,
+      100
+    )
+  );
   maxAmpFill.style.width = `${maxAmpPercentage}%`;
   maxAmpText.textContent = `${maxAmpPercentage}%`;
 }
